7e2588c9e1dbc2dd24b5a21a26e1cd5cd17e55b7
benjamin kunkel <benjamink1125@gmail.com>
2022-10-21 11:37:27 -0700

    finished

---
+++ Iteration/Program.cs
@@ -0,0 +1,41 @@
+﻿var numbers = new List<int>();
+
+
+var myNumber = 0;
+
+do
+{
+    myNumber++;
+    numbers.Add(myNumber);
+}while (myNumber< 100);
+
+    while (myNumber < 200)
+{
+    myNumber++;
+
+    numbers.Add(myNumber);
+}
+    
+
+    Console.WriteLine("increase:");
+
+foreach (var item in numbers)
+
+{
+    Console.WriteLine($"{item}");
+
+}
+
+    Console.WriteLine("");
+    Console.WriteLine("decrease:");
+
+
+
+   for (var i = 199; i >= 0;i--)
+
+        Console.WriteLine($"{numbers[i]}");
+
+
+
+
+
